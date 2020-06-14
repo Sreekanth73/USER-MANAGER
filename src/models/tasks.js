d368dcdc2c2b531df942d5876cf272bb446ab158
pathisreekanth <36104430+pathisreekanth@users.noreply.github.com>
2020-06-14 16:40:30 +0530

    Added refernce user and timestamps

--- src/models/tasks.js
+++ src/models/tasks.js
@@ -1,15 +1,25 @@
 const mongoose = require("mongoose");
 
-const Task = new mongoose.model("Task", {
-  description: {
-    type: String,
-    required: true,
-    trim: true,
+const taskSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
   },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = Task;
+module.exports = mongoose.model("Task", taskSchema);
