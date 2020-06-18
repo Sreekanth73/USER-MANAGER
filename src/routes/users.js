f9cab0996b3bb222eda43f3dfddf264f772277bb
pathisreekanth <36104430+pathisreekanth@users.noreply.github.com>
2020-06-18 19:01:20 +0530

    Users Edit and Delete

--- src/routes/users.js
+++ src/routes/users.js
@@ -15,7 +15,9 @@ router.post("/signUp", async (req, res) => {
     await user.save();
     res.redirect("/login");
   } catch (e) {
-    res.render("/signup");
+    res.render("signup", {
+      message: e.message,
+    });
   }
 });
 
@@ -79,41 +81,33 @@ router.get("/users/findUser", async (req, res) => {
     }
   }
 });
-router.patch("/users/:id", async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ["name", "email", "password", "age"];
-  const isValidOperation = updates.every((update) =>
-    allowedUpdates.includes(update)
-  );
-  console.log(isValidOperation);
-
-  if (!isValidOperation) {
-    return res.status(400).send({ error: "error" });
-  }
-
-  const _id = req.params.id;
-
+router.post("/users/edit/:id", async (req, res) => {
   try {
-    const user = await User.findByIdAndUpdate(_id);
-    updates.forEach((update) => {
-      user[update] = req.body[update];
+    const user = await User.findOneAndUpdate({ _id: req.params.id }, req.body, {
+      new: true,
     });
     await user.save();
-    res.status(201).send(user);
+    res.redirect("/users");
   } catch (e) {
     res.status(404).send(e);
   }
 });
 
-router.delete("/users/:id", async (req, res) => {
+router.get("/users/edit/:id", async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) {
-      res.status(404).send("no user found");
-    }
-    res.status(200).send(user);
+    const user = await User.findById(req.params.id);
+    res.render("edit", { user });
   } catch (e) {
-    res.status(500).send(e);
+    res.send(e);
+  }
+});
+
+router.get("/users/delete/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndRemove(req.params.id);
+    res.redirect("/users");
+  } catch (e) {
+    res.status(500).send(e.message);
   }
 });
 
